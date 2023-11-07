d22b01c6f9095f7b19121ae4cd1758ca9737b544
tyVespA <migliavacca.marco08@gmail.com>
2023-11-07 17:08:42 +0100

    Add nav mobile logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Navbar.module.css";
 import Logo from "../assets/images/logo.svg";
 import Button from "./Button";
+import HamburgerIcon from "../assets/images/icon-hamburger.svg";
+import CloseNavIcon from "../assets/images/icon-close.svg";
 
 function Navbar() {
+  const [openMobileMenu, setOpenMobileMenu] = useState(false);
+  function setMobileMenu() {
+    setOpenMobileMenu(!openMobileMenu);
+  }
+
   return (
     <header className={styles.header}>
       <img src={Logo} alt="logo" />
-      <nav>
+      <nav className={openMobileMenu ? styles.fadedIn : styles.fadedOut}>
         <ul>
           <li>
             <a href="#">Pricing</a>
@@ -26,8 +33,25 @@ function Navbar() {
           </li>
         </ul>
       </nav>
-      {/* <button>Get Started</button> */}
-      <Button type="primary">Get Started</Button>
+      <div className={styles.ctaBtn}>
+        <Button type="primary">Get Started</Button>
+      </div>
+      <div className={styles.mobileNavBtn}>
+        <button onClick={setMobileMenu}>
+          <img
+            className={openMobileMenu ? styles.hidden : styles.visible}
+            src={HamburgerIcon}
+            alt="menu icon"
+          />
+        </button>
+        <button onClick={setMobileMenu}>
+          <img
+            className={openMobileMenu ? styles.visible : styles.hidden}
+            src={CloseNavIcon}
+            alt="close menu icon"
+          />
+        </button>
+      </div>
     </header>
   );
 }
